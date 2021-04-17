7d821a28b809427d5856023df36c0cc961309820
arporral <arporral@gmail.com>
2021-04-17 19:36:11 +0200

    17-04-2021

--- src/main/java/com/titus/cargarcarreras/CargarCarreras.java
+++ src/main/java/com/titus/cargarcarreras/CargarCarreras.java
@@ -7,7 +7,9 @@ package com.titus.cargarcarreras;
 
 import com.google.gson.Gson;
 import com.titus.carreras.Activities;
+import com.titus.carreras.Carreirasnrc;
 import com.titus.carreras.Matriz;
+import com.titus.carreras.Summaries;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -38,7 +40,7 @@ public class CargarCarreras {
         try {
             // Apertura del fichero y creacion de BufferedReader para poder
             // hacer una lectura comoda (disponer del metodo readLine()).
-            archivo = new File("F:\\prueba-0.json");
+            archivo = new File("F:\\ANALISIS Y PROGRAMACION\\Programas java\\CargarCarrerasDesdeNRC\\resources\\prueba-0.json");
             fr = new FileReader(archivo);
             br = new BufferedReader(fr);
 
@@ -89,24 +91,75 @@ public class CargarCarreras {
             
             Activities nrc = (Activities) it.next();
 
-//            Carreiras c = new Carreiras();
-//            c.setFecha(new Date(nrc.getStart_epoch_ms()));            
-//            em.persist(c);
-            
+            // Fecha
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             sdf.setTimeZone(TimeZone.getTimeZone("UTC"));            
             Date fecha = new Date(nrc.getStart_epoch_ms().longValue());
             System.out.println("Fecha = " + sdf.format(fecha));            
             
+            // Duracion
 //            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MMM-dd HH:mm:ss");
             sdf = new SimpleDateFormat("HH:mm:ss");
             sdf.setTimeZone(TimeZone.getTimeZone("UTC"));  
             Date duracion = new Date(nrc.getActive_duration_ms().longValue());
             System.out.println("Duracion = " + sdf.format(duracion));          
 
+            // Kms, calorias y pasos (se encuentran en el array de summaries).
+            double kms = 0.0;
+            double calorias = 0.0;
+            int pasos = 0;
+            
+            for (Summaries summary : nrc.getSummaries()) {
+                if (summary.getMetric().equals("distance")){
+                    kms = summary.getValue();
+                }
+                if (summary.getMetric().equals("calories")){
+                    calorias = summary.getValue();
+                }
+                if (summary.getMetric().equals("steps")){
+                    pasos = (int) summary.getValue();
+                }
+            }
+
+            System.out.println("Kms = " + kms);
+            System.out.println("Calorias = " + calorias);
+            System.out.println("Pasos = " + pasos);
+            
+            // recorrido, sensaciones, calzado, clima, terreno y temperatura (se encuentran en el array de tags).
+            String nombre = nrc.getTags().getComNikeName();
+            String sensaciones = nrc.getTags().getEmotion();
+            String calzado = (nrc.getTags().getShoes() == null)? nrc.getTags().getShoe_id():nrc.getTags().getShoes();
+            String terreno = nrc.getTags().getTerrain();
+            String clima = (nrc.getTags().getWeather() == null)? nrc.getTags().getComNikeWeather():nrc.getTags().getWeather();          
+            String temperatura = (nrc.getTags().getTemperature() == null)? nrc.getTags().getComNikeTemperature():nrc.getTags().getTemperature();            
+            
+            System.out.println("Nombre = " + nombre);
+            System.out.println("Sensaciones = " + sensaciones);
+            System.out.println("Calzado = " + calzado);
+            System.out.println("Terreno = " + terreno);
+            System.out.println("Clima = " + clima);
+            System.out.println("Temperatura = " + temperatura);
+            
+//            Carreirasnrc c = new Carreirasnrc();
+//            c.setFecha(fecha);            
+//            c.setDuracion(duracion);  
+//            c.setKms(kms);
+//            c.setRecorrido(nombre);
+//            c.setTipoDeEjercicio("correr");
+//            c.setSensaciones(sensaciones);
+//            c.setClima(clima);
+//            c.setCalzado(calzado);
+//            c.setCalorias(calorias);
+//            c.setPasos(pasos);
+//            c.setTemperatura(temperatura);
+//            c.setTerreno(terreno);
+//            
+//            em.persist(c);            
+
             num++;
         }
         tx.commit();
         em.close();
+        System.out.println("Se han insertado " + num + " en la tabla Carreirasnrc.");
     }    
 }
---
+++ src/main/java/com/titus/carreras/Carreirasnrc.java
@@ -0,0 +1,235 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.titus.carreras;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author arpor
+ */
+@Entity
+@Table(name = "carreirasnrc")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Carreirasnrc.findAll", query = "SELECT c FROM Carreirasnrc c"),
+    @NamedQuery(name = "Carreirasnrc.findByNumSec", query = "SELECT c FROM Carreirasnrc c WHERE c.numSec = :numSec"),
+    @NamedQuery(name = "Carreirasnrc.findByFecha", query = "SELECT c FROM Carreirasnrc c WHERE c.fecha = :fecha"),
+    @NamedQuery(name = "Carreirasnrc.findByDuracion", query = "SELECT c FROM Carreirasnrc c WHERE c.duracion = :duracion"),
+    @NamedQuery(name = "Carreirasnrc.findByKms", query = "SELECT c FROM Carreirasnrc c WHERE c.kms = :kms"),
+    @NamedQuery(name = "Carreirasnrc.findByTipoDeEjercicio", query = "SELECT c FROM Carreirasnrc c WHERE c.tipoDeEjercicio = :tipoDeEjercicio"),
+    @NamedQuery(name = "Carreirasnrc.findByPeso", query = "SELECT c FROM Carreirasnrc c WHERE c.peso = :peso"),
+    @NamedQuery(name = "Carreirasnrc.findByCalorias", query = "SELECT c FROM Carreirasnrc c WHERE c.calorias = :calorias"),
+    @NamedQuery(name = "Carreirasnrc.findByPasos", query = "SELECT c FROM Carreirasnrc c WHERE c.pasos = :pasos"),
+    @NamedQuery(name = "Carreirasnrc.findBySensaciones", query = "SELECT c FROM Carreirasnrc c WHERE c.sensaciones = :sensaciones"),
+    @NamedQuery(name = "Carreirasnrc.findByCalzado", query = "SELECT c FROM Carreirasnrc c WHERE c.calzado = :calzado"),
+    @NamedQuery(name = "Carreirasnrc.findByTerreno", query = "SELECT c FROM Carreirasnrc c WHERE c.terreno = :terreno"),
+    @NamedQuery(name = "Carreirasnrc.findByClima", query = "SELECT c FROM Carreirasnrc c WHERE c.clima = :clima"),
+    @NamedQuery(name = "Carreirasnrc.findByTemperatura", query = "SELECT c FROM Carreirasnrc c WHERE c.temperatura = :temperatura")})
+public class Carreirasnrc implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "num_sec")
+    private Integer numSec;
+    @Basic(optional = false)
+    @Column(name = "fecha")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Column(name = "duracion")
+    @Temporal(TemporalType.TIME)
+    private Date duracion;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "kms")
+    private Double kms;
+    @Lob
+    @Column(name = "recorrido")
+    private String recorrido;
+    @Column(name = "tipo_de_ejercicio")
+    private String tipoDeEjercicio;
+    @Column(name = "peso")
+    private Double peso;
+    @Column(name = "calorias")
+    private Double calorias;
+    @Column(name = "pasos")
+    private Integer pasos;
+    @Column(name = "sensaciones")
+    private String sensaciones;
+    @Column(name = "calzado")
+    private String calzado;
+    @Column(name = "terreno")
+    private String terreno;
+    @Column(name = "clima")
+    private String clima;
+    @Column(name = "temperatura")
+    private String temperatura;
+
+    public Carreirasnrc() {
+    }
+
+    public Carreirasnrc(Integer numSec) {
+        this.numSec = numSec;
+    }
+
+    public Carreirasnrc(Integer numSec, Date fecha) {
+        this.numSec = numSec;
+        this.fecha = fecha;
+    }
+
+    public Integer getNumSec() {
+        return numSec;
+    }
+
+    public void setNumSec(Integer numSec) {
+        this.numSec = numSec;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Date getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Date duracion) {
+        this.duracion = duracion;
+    }
+
+    public Double getKms() {
+        return kms;
+    }
+
+    public void setKms(Double kms) {
+        this.kms = kms;
+    }
+
+    public String getRecorrido() {
+        return recorrido;
+    }
+
+    public void setRecorrido(String recorrido) {
+        this.recorrido = recorrido;
+    }
+
+    public String getTipoDeEjercicio() {
+        return tipoDeEjercicio;
+    }
+
+    public void setTipoDeEjercicio(String tipoDeEjercicio) {
+        this.tipoDeEjercicio = tipoDeEjercicio;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Double getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(Double calorias) {
+        this.calorias = calorias;
+    }
+
+    public Integer getPasos() {
+        return pasos;
+    }
+
+    public void setPasos(Integer pasos) {
+        this.pasos = pasos;
+    }
+
+    public String getSensaciones() {
+        return sensaciones;
+    }
+
+    public void setSensaciones(String sensaciones) {
+        this.sensaciones = sensaciones;
+    }
+
+    public String getCalzado() {
+        return calzado;
+    }
+
+    public void setCalzado(String calzado) {
+        this.calzado = calzado;
+    }
+
+    public String getTerreno() {
+        return terreno;
+    }
+
+    public void setTerreno(String terreno) {
+        this.terreno = terreno;
+    }
+
+    public String getClima() {
+        return clima;
+    }
+
+    public void setClima(String clima) {
+        this.clima = clima;
+    }
+
+    public String getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(String temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (numSec != null ? numSec.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Carreirasnrc)) {
+            return false;
+        }
+        Carreirasnrc other = (Carreirasnrc) object;
+        if ((this.numSec == null && other.numSec != null) || (this.numSec != null && !this.numSec.equals(other.numSec))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.titus.carreras.Carreirasnrc[ numSec=" + numSec + " ]";
+    }
+    
+}
